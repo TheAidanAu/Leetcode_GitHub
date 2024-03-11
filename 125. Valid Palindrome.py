ed48a2783d2554581847c4452bb428c5c19a5843
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-11 18:02:50 -0400

    125. Valid Palindrome

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -8,15 +8,18 @@ class Solution:
         if len(converted) == 0 or len(converted) == 1:
             return True
 
-        start = 0
-        end = len(converted) - 1
-        while start < end:
-            if converted[start] == converted[end]:
-                start += 1
-                end -= 1
-            else:
-                return False
-        return True
+        # or just check if the reversed string is the same as the original 
+        return converted == converted[::-1]
+
+        # start = 0
+        # end = len(converted) - 1
+        # while start < end:
+        #     if converted[start] == converted[end]:
+        #         start += 1
+        #         end -= 1
+        #     else:
+        #         return False
+        # return True
 
 # Time:  O(N+N/2) = N(N)
 # Space: O(n), n<=N , n=N if all characters are alphabets
