316ec05516b8cc06f114288dfce8c03ea8bb7946
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-12 14:06:11 -0400

    125. Valid Palindrome

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,25 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        converted = ""
+
+        # remove non-alphanumeric characters
+        # convert to lower cases
+
+        converted = []
+
         for char in s:
             if char.isalnum():
-                converted += char.lower()
+                converted.append(char.lower())
+
+        # join all char in the array with an empty delineator
+        converted = "".join(converted)
 
         if len(converted) == 0 or len(converted) == 1:
             return True
 
-        # or just check if the reversed string is the same as the original 
-        return converted == converted[::-1]
+        return converted == converted[:: -1]
 
-        # start = 0
-        # end = len(converted) - 1
-        # while start < end:
-        #     if converted[start] == converted[end]:
-        #         start += 1
-        #         end -= 1
-        #     else:
-        #         return False
-        # return True
-
-# Time:  O(N+N/2) = N(N)
+# Time:  O(N)
 # Space: O(n), n<=N , n=N if all characters are alphabets
