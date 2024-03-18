39a9411db8381a666a6f827dfd89607bd5acbdbd
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-18 16:21:30 -0400

    20% of Blind 75 done

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -5,17 +5,16 @@ class Solution:
         # if the mid point val is too big, shift the r pointer to the mid
         l, r = 0, len(nums) - 1
 
+        # it's possible that both pointers meet as they converge
         while l <= r:
             m = (l + r) // 2
-
             if nums[m] > target:
-                r = m - 1
+                r = m - 1  # because we alread looked at m
             elif nums[m] < target:
-                l = m + 1
+                l = m + 1  # because we alread looked at m
             else:
                 return m
         return -1
-
     # time: O(logN) search space is halved at each iteration
     # space: O(1)
 
