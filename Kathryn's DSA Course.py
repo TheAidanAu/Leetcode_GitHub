177ddeebbab27e114efb075cfc3d13268d7841e1
Aidan Au <aidan.au.linkedin@gmail.com>
2024-03-25 09:18:40 -0400

    Added solution

--- Kathryn's DSA Course.py
+++ Kathryn's DSA Course.py
@@ -1,6 +1,20 @@
 # Queue/Stack
 # Use Deque as a Queue
 
+from collections import deque
+
+printer_queue = deque()
+
+#enque aka add item to the queue
+printer_queue.append("taylortickets.pdf")
+printer_queue.append("notes.pdf")
+printer_queue.append("id.pdf")
+
+# Use the popleft method to remove item from the front
+while printer_queue:
+    doc = printer_queue.popleft() #FIFO
+    print(doc)
+
 
 
 
@@ -8,3 +22,12 @@
 
 # Use list as a Stack
 book_stack = []
+
+book_stack.append("jack of hearts")
+book_stack.append("2 of diamonds")
+book_stack.append("10 of spades")
+
+top_card = book_stack.pop()  # 10 of spades
+print(top_card)
+
+top_card = book_stack[-1]  # 2 of diamond
