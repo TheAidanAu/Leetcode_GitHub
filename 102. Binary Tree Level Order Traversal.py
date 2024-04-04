c049ed9d79758876a80330ee5cadf2621c75dac3
Aidan Au <aidan.au.linkedin@gmail.com>
2024-04-04 05:30:12 -0400

    Added new solution

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -11,14 +11,17 @@ class Solution:
         # append each node and recursively call the function to the left & right child and increase the level
         # Call this helper function with the root node and level 0
         # The resulting values of the d is the expected output
+        # (It traverses the binary tree by going deeper into the left subtree first before moving to the right subtree.
+        # the left child of the current node is explored first, followed by the right child.)
 
         d = defaultdict(list)
 
         def dfs(node, level):
             if not node:
                 return
-
             d[level].append(node.val)
+            # It traverses the binary tree by going deeper into the left subtree first before moving to the right subtree.
+            # the left child of the current node is explored first, followed by the right child.
             dfs(node.left, level + 1)
             dfs(node.right, level + 1)
 
