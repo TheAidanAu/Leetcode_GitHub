747485e5d9ce98a9d6788b47e7a224735f6f0d4b
Aidan Au <aidan.au.linkedin@gmail.com>
2024-06-03 06:12:55 -0400

    Added new solution

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -20,4 +20,4 @@ class Solution:
 
         dfs(0)
         return res
-#time: O(2^n) 2 times more choice at each level                                        
+#time: O(2^n) 2 times more choice at each level                                      
